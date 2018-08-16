1607de78cec9d3cd6be2330acb145e694cd5df0c
Tom Chege <emdeechege@gmail.com>
2018-08-16 04:49:20 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GiphyComponent } from './giphy/giphy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GiphyComponent
   ],
   imports: [
     BrowserModule
